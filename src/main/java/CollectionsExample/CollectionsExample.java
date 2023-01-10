6813e385167f0fdf4fb904e47ad10bb7b7e6b9b2
gold_roger <1rn21cs016.akash@gmail.com>
2023-01-11 00:31:12 +0530

    Insertion sort completed

---
+++ src/main/java/CollectionsExample/CollectionsExample.java
@@ -0,0 +1,8 @@
+package DSA.src.main.java.CollectionsExample;//package CollectionsExample;
+
+public class CollectionsExample {
+
+
+
+
+}
--- src/main/java/CollectionsExample/collectionsExample.java
+++
@@ -1,8 +0,0 @@
-package CollectionsExample;
-
-public class collectionsExample {
-
-
-
-
-}
